3939d06042339d4a9f0109345c77659401ddfc68
dantoth24 <dantoth24@gmail.com>
2018-02-08 18:05:54 -0800

    Stuff:

---
+++ SurvivalShooter v2.0/Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingSceen;
+    public Slider slider;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+        loadingSceen.SetActive(true);
+    
+    }
+
+    IEnumerator LoadAsync (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+	
+}
